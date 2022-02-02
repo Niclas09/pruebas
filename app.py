ed5fee3cede259b2e2a213c31e62a97b37f2519b
Niclas09 <98900672+Niclas09@users.noreply.github.com>
2022-02-02 11:12:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import time
 import base64
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
 import geopy
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
