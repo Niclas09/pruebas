80c9782d1f6fa9d38846c07c321bdbca6d498651
Niclas09 <98900672+Niclas09@users.noreply.github.com>
2022-02-02 11:01:21 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import time
+import base64
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+import plotly_express as px
